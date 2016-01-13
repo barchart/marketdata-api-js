6dc0b319910ded12dd38d8bdfff7c2bb999e467d
Bryan Ingle <bryan.ingle@barchart.com>
2016-01-13 12:14:53 -0600

    Price formatter now handles NaN, null, undefined

--- test/specs/util/priceFormatterSpec.js
+++ test/specs/util/priceFormatterSpec.js
@@ -23,6 +23,22 @@ describe('When a price formatter is created', function() {
         it('formats 0 (with unit code 2) as "0-0"', function() {
             expect(priceFormatter.format(0, '2')).toEqual('0-0');
         });
+
+        it('formats zero-length string (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format('', '2')).toEqual('');
+        });
+
+        it('formats undefined (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(undefined, '2')).toEqual('');
+        });
+
+        it('formats null (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(null, '2')).toEqual('');
+        });
+
+        it('formats Number.NaN (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(Number.NaN, '2')).toEqual('');
+        });
     });
 
     describe('with a tick fraction separator and no special fractions', function() {
@@ -44,6 +60,22 @@ describe('When a price formatter is created', function() {
 
         it('formats 0 (with unit code 2) as "0\'0"', function() {
             expect(priceFormatter.format(0, '2')).toEqual('0\'0');
+        });
+
+        it('formats zero-length string (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format('', '2')).toEqual('');
+        });
+
+        it('formats undefined (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(undefined, '2')).toEqual('');
+        });
+
+        it('formats null (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(null, '2')).toEqual('');
+        });
+
+        it('formats Number.NaN (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(Number.NaN, '2')).toEqual('');
         });
     });
 
@@ -68,5 +100,20 @@ describe('When a price formatter is created', function() {
             expect(priceFormatter.format(0, '2')).toEqual('0');
         });
 
+        it('formats zero-length string (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format('', '2')).toEqual('');
+        });
+
+        it('formats undefined (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(undefined, '2')).toEqual('');
+        });
+
+        it('formats null (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(null, '2')).toEqual('');
+        });
+
+        it('formats Number.NaN (with unit code 2) as zero-length string', function() {
+            expect(priceFormatter.format(Number.NaN, '2')).toEqual('');
+        });
     });
 });
