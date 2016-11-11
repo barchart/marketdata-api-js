3dd38ed1a9bb839d8a2667534b28b0dd308a2f7f
Bryan Ingle <bryan.ingle@barchart.com>
2016-11-11 16:01:30 -0600

    Release. Bump version number

--- dist/barchart-marketdata-marketstate-1.0.js
+++ dist/barchart-marketdata-marketstate-1.0.js
@@ -517,14 +517,15 @@ module.exports = function() {
 	'use strict';
 
 	return function(dayCode) {
-		var val1 = dayCode.charCodeAt(0);
+		var d = parseInt(dayCode, 31);
 
-		if ((val1 >= ("1").charCodeAt(0)) && (dayCode <= ("9").charCodeAt(0)))
-			return (val1 - ("0").charCodeAt(0));
-		else if (dayCode == "0")
-			return 10;
-		else
-			return ((val1 - ("A").charCodeAt(0)) + 11);
+		if (d > 9) {
+			d++;
+		} else if (d == 0) {
+			d = 10;
+		}
+
+		return d;
 	};
 }();
 },{}],10:[function(require,module,exports){
