03679cb9c9486ab21ad75f57e16308ad77a9c23b
Bryan Ingle <bryan.ingle@barchart.com>
2016-11-16 17:04:29 -0600

    Cumulative Volume (WIP)

---
+++ test/specs/marketState/CumulativeVolumeSpec.js
@@ -0,0 +1,274 @@
+var CumulativeVolume = require('../../../lib/marketState/CumulativeVolume');
+
+describe('When a cumulative volume container is created with a tick increment of 0.25', function() {
+	var cv;
+
+	var symbol;
+	var tickIncrement;
+
+	beforeEach(function() {
+		cv = new CumulativeVolume(symbol = 'ESZ6', tickIncrement = 0.25);
+	});
+
+	it('the symbol should be the same value as assigned during construction', function() {
+		expect(cv.symbol).toEqual(symbol);
+	});
+
+	it('the price level array should contain zero items', function() {
+		expect(cv.toArray().length).toEqual(0);
+	});
+
+	describe('and 50 contracts are traded at 2172.50', function() {
+		beforeEach(function() {
+			cv.incrementVolume(2172.5, 50);
+		});
+
+		it('should report zero contracts traded at 2172.25', function() {
+			expect(cv.getVolume(2172.25)).toEqual(0);
+		});
+
+		it('should report 50 contracts traded at 2172.50', function() {
+			expect(cv.getVolume(2172.5)).toEqual(50);
+		});
+
+		it('should report zero contracts traded at 2172.75', function() {
+			expect(cv.getVolume(2172.75)).toEqual(0);
+		});
+
+		describe('and an price level array is retrieved', function() {
+			var priceLevels;
+
+			beforeEach(function() {
+				priceLevels = cv.toArray();
+			});
+
+			it('the price level array should contain one item', function() {
+				expect(priceLevels.length).toEqual(1);
+			});
+
+			it('the first price level item should be for 50 contracts', function() {
+				expect(priceLevels[0].volume).toEqual(50);
+			});
+
+			it('the first price level item should be priced at 2172.50', function() {
+				expect(priceLevels[0].price).toEqual(2172.5);
+			});
+		});
+
+		describe('and another 50 contracts are traded at 2172.50', function() {
+			beforeEach(function() {
+				cv.incrementVolume(2172.5, 50);
+			});
+
+			it('should report zero contracts traded at 2172.25', function() {
+				expect(cv.getVolume(2172.25)).toEqual(0);
+			});
+
+			it('should report 50 contracts traded at 2172.50', function() {
+				expect(cv.getVolume(2172.5)).toEqual(100);
+			});
+
+			it('should report zero contracts traded at 2172.75', function() {
+				expect(cv.getVolume(2172.75)).toEqual(0);
+			});
+
+			describe('and an price level array is retrieved', function() {
+				var priceLevels;
+
+				beforeEach(function() {
+					priceLevels = cv.toArray();
+				});
+
+				it('the price level array should contain one item', function() {
+					expect(priceLevels.length).toEqual(1);
+				});
+
+				it('the first price level item should be for 100 contracts', function() {
+					expect(priceLevels[0].volume).toEqual(100);
+				});
+
+				it('the first price level item should be priced at 2172.50', function() {
+					expect(priceLevels[0].price).toEqual(2172.5);
+				});
+			});
+		});
+
+		describe('and 200 contracts are traded at 2172.25', function() {
+			beforeEach(function() {
+				cv.incrementVolume(2172.25, 200);
+			});
+
+			it('should report 200 contracts traded at 2172.25', function() {
+				expect(cv.getVolume(2172.25)).toEqual(200);
+			});
+
+			it('should report 50 contracts traded at 2172.50', function() {
+				expect(cv.getVolume(2172.5)).toEqual(50);
+			});
+
+			it('should report zero contracts traded at 2172.75', function() {
+				expect(cv.getVolume(2172.75)).toEqual(0);
+			});
+
+			describe('and an price level array is retrieved', function() {
+				var priceLevels;
+
+				beforeEach(function() {
+					priceLevels = cv.toArray();
+				});
+
+				it('the price level array should contain two items', function() {
+					expect(priceLevels.length).toEqual(2);
+				});
+
+				it('the first price level item should be for 200 contracts', function() {
+					expect(priceLevels[0].volume).toEqual(200);
+				});
+
+				it('the first price level item should be priced at 2172.25', function() {
+					expect(priceLevels[0].price).toEqual(2172.25);
+				});
+
+				it('the second price level item should be for 50 contracts', function() {
+					expect(priceLevels[1].volume).toEqual(50);
+				});
+
+				it('the second price level item should be priced at 2172.50', function() {
+					expect(priceLevels[1].price).toEqual(2172.5);
+				});
+			});
+		});
+
+		describe('and 3 contracts are traded at 2173.50', function() {
+			beforeEach(function() {
+				cv.incrementVolume(2173.50, 3);
+			});
+
+			it('should report 50 contracts traded at 2172.50', function() {
+				expect(cv.getVolume(2172.5)).toEqual(50);
+			});
+
+			it('should report zero contracts traded at 2172.75', function() {
+				expect(cv.getVolume(2172.75)).toEqual(0);
+			});
+
+			it('should report zero contracts traded at 2173', function() {
+				expect(cv.getVolume(2173)).toEqual(0);
+			});
+
+			it('should report zero contracts traded at 2173.25', function() {
+				expect(cv.getVolume(2173.25)).toEqual(0);
+			});
+
+			it('should report 3 contracts traded at 2173.50', function() {
+				expect(cv.getVolume(2173.50)).toEqual(3);
+			});
+
+			describe('and an price level array is retrieved', function() {
+				var priceLevels;
+
+				beforeEach(function() {
+					priceLevels = cv.toArray();
+				});
+
+				it('the price level array should contain five items', function() {
+					expect(priceLevels.length).toEqual(5);
+				});
+
+				it('the first price level item should be for 50 contracts', function() {
+					expect(priceLevels[0].volume).toEqual(50);
+				});
+
+				it('the first price level item should be priced at 2172.50', function() {
+					expect(priceLevels[0].price).toEqual(2172.5);
+				});
+
+				it('the second price level item should be for zero contracts', function() {
+					expect(priceLevels[1].volume).toEqual(0);
+				});
+
+				it('the second price level item should be priced at 2172.75', function() {
+					expect(priceLevels[1].price).toEqual(2172.75);
+				});
+
+				it('the third price level item should be for zero contracts', function() {
+					expect(priceLevels[2].volume).toEqual(0);
+				});
+
+				it('the third price level item should be priced at 2173.00', function() {
+					expect(priceLevels[2].price).toEqual(2173);
+				});
+
+				it('the fourth price level item should be for zero contracts', function() {
+					expect(priceLevels[3].volume).toEqual(0);
+				});
+
+				it('the fourth price level item should be priced at 2173.25', function() {
+					expect(priceLevels[3].price).toEqual(2173.25);
+				});
+
+				it('the fifth price level item should be for 3 contracts', function() {
+					expect(priceLevels[4].volume).toEqual(3);
+				});
+
+				it('the fifth price level item should be priced at 2173.50', function() {
+					expect(priceLevels[4].price).toEqual(2173.5);
+				});
+			});
+		});
+
+		describe('and 99 contracts are traded at 2172.00', function() {
+			beforeEach(function() {
+				cv.incrementVolume(2172.00, 99);
+			});
+
+			it('should report 99 contracts traded at 2172.00', function() {
+				expect(cv.getVolume(2172.00)).toEqual(99);
+			});
+
+			it('should report zero contracts traded at 2172.25', function() {
+				expect(cv.getVolume(2172.25)).toEqual(0);
+			});
+
+			it('should report 50 contracts traded at 2172.50', function() {
+				expect(cv.getVolume(2172.50)).toEqual(50);
+			});
+
+			describe('and an price level array is retrieved', function() {
+				var priceLevels;
+
+				beforeEach(function() {
+					priceLevels = cv.toArray();
+				});
+
+				it('the price level array should contain three items', function() {
+					expect(priceLevels.length).toEqual(3);
+				});
+
+				it('the first price level item should be for 99 contracts', function() {
+					expect(priceLevels[0].volume).toEqual(99);
+				});
+
+				it('the first price level item should be priced at 2172.00', function() {
+					expect(priceLevels[0].price).toEqual(2172);
+				});
+
+				it('the second price level item should be for zero contracts', function() {
+					expect(priceLevels[1].volume).toEqual(0);
+				});
+
+				it('the second price level item should be priced at 2172.25', function() {
+					expect(priceLevels[1].price).toEqual(2172.25);
+				});
+
+				it('the third price level item should be for 50 contracts', function() {
+					expect(priceLevels[2].volume).toEqual(50);
+				});
+
+				it('the third price level item should be priced at 2172.50', function() {
+					expect(priceLevels[2].price).toEqual(2172.50);
+				});
+			});
+		});
+	});
+});
