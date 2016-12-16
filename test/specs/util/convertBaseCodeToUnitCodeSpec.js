399930828df543c2cc1661f188f9f6c41314b468
Bryan Ingle <bryan.ingle@barchart.com>
2016-12-16 14:38:19 -0600

    Remove unit tests that belong in marketdata-utilities-js library

--- test/specs/util/convertBaseCodeToUnitCodeSpec.js
+++
@@ -1,7 +0,0 @@
-var convertBaseCodeToUnitCode = require('../../../lib/util/convertBaseCodeToUnitCode');
-
-describe('When converting a baseCode to a unitCode', function() {
-	it('-1 should translate to "2"', function() {
-		expect(convertBaseCodeToUnitCode(-1)).toEqual('2');
-	});
-});
--- test/specs/util/convertUnitCodeToBaseCodeSpec.js
+++
@@ -1,7 +0,0 @@
-var convertUnitCodeToBaseCode = require('../../../lib/util/convertUnitCodeToBaseCode');
-
-describe('When converting a unitCode to a baseCode', function() {
-	it('"2" should translate to -1', function() {
-		expect(convertUnitCodeToBaseCode('2')).toEqual(-1);
-	});
-});
