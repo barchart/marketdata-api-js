8b418ae048dd6c9577134024af8c4b23cb7bedda
Bryan Ingle <bryan.ingle@barchart.com>
2019-09-30 18:03:45 -0500

    Fix for 10-Yr T-Note (when negative & string.length = 7, see code)

--- test/specs/utilities/convert/stringToDecimalSpec.js
+++ test/specs/utilities/convert/stringToDecimalSpec.js
@@ -8,8 +8,16 @@ describe('when parsing prices', () => {
 			expect(convertStringToDecimal('125-5', '2')).toEqual(125.625);
 		});
 
+		it('returns -125.625 (with unit code 2) when parsing "-125-5"', () => {
+			expect(convertStringToDecimal('-125-5', '2')).toEqual(-125.625);
+		});
+
 		it('returns 125.625 (with unit code 5) when parsing "125-240"', () => {
 			expect(convertStringToDecimal('125-240', '5')).toEqual(125.75);
+		});
+
+		it('returns -125.625 (with unit code 5) when parsing "-125-240"', () => {
+			expect(convertStringToDecimal('-125-240', '5')).toEqual(-125.75);
 		});
 	});
 });
