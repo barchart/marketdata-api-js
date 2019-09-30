a2985cbe124eb98f988ee8b3943b36f27f2313ba
Bryan Ingle <bryan.ingle@barchart.com>
2019-09-30 17:36:26 -0500

    WIP

--- test/specs/utilities/convert/stringToDecimalSpec.js
+++ test/specs/utilities/convert/stringToDecimalSpec.js
@@ -1,17 +1,15 @@
-/*
-let stringToDecimalFormatterSpec = require('./../../../lib/util/stringToDecimalFormatter');
+const convertStringToDecimal = require('./../../../../lib/utilities/convert/stringToDecimal');
 
 describe('when parsing prices', () => {
 	'use strict';
 
 	describe('with a fractional separator', () => {
 		it('returns 125.625 (with unit code 2) when parsing "125-5"', () => {
-			expect(stringToDecimalFormatterSpec('125-5', '2')).toEqual(125.625);
+			expect(convertStringToDecimal('125-5', '2')).toEqual(125.625);
 		});
 
 		it('returns 125.625 (with unit code 5) when parsing "125-240"', () => {
-			expect(stringToDecimalFormatterSpec('125-240', '5')).toEqual(125.75);
+			expect(convertStringToDecimal('125-240', '5')).toEqual(125.75);
 		});
 	});
 });
-*/
