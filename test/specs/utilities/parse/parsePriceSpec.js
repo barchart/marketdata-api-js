5bfbb6bb91ee8f3b8b1afdfb8b983daeff2f0fdd
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-03-31 13:18:23 -0500

    Move unit tests from aliased "convertStringToDecimal" to new "parsePrice"

--- test/specs/utilities/parse/parsePriceSpec.js
+++ test/specs/utilities/parse/parsePriceSpec.js
@@ -1,23 +1,23 @@
-const convertStringToDecimal = require('./../../../../lib/utilities/convert/stringToDecimal');
+const parsePrice = require('../../../../lib/utilities/parse/price');
 
 describe('when parsing prices', () => {
 	'use strict';
 
 	describe('with a fractional separator', () => {
 		it('returns 125.625 (with unit code 2) when parsing "125-5"', () => {
-			expect(convertStringToDecimal('125-5', '2')).toEqual(125.625);
+			expect(parsePrice('125-5', '2')).toEqual(125.625);
 		});
 
 		it('returns -125.625 (with unit code 2) when parsing "-125-5"', () => {
-			expect(convertStringToDecimal('-125-5', '2')).toEqual(-125.625);
+			expect(parsePrice('-125-5', '2')).toEqual(-125.625);
 		});
 
 		it('returns 125.625 (with unit code 5) when parsing "125-240"', () => {
-			expect(convertStringToDecimal('125-240', '5')).toEqual(125.75);
+			expect(parsePrice('125-240', '5')).toEqual(125.75);
 		});
 
 		it('returns -125.625 (with unit code 5) when parsing "-125-240"', () => {
-			expect(convertStringToDecimal('-125-240', '5')).toEqual(-125.75);
+			expect(parsePrice('-125-240', '5')).toEqual(-125.75);
 		});
 	});
 });
