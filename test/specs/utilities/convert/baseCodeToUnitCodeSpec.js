748407289df0f390cd40cc21a2a9003b515b9538
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-03-31 13:10:26 -0500

    Unknown "base" codes will convert to "0" (string) not 0 (number)

--- test/specs/utilities/convert/baseCodeToUnitCodeSpec.js
+++ test/specs/utilities/convert/baseCodeToUnitCodeSpec.js
@@ -57,15 +57,15 @@ describe('When converting a baseCode to a unitCode', () => {
 		expect(convertBaseCodeToUnitCode(7)).toEqual('F');
 	});
 
-	it('"-1" should translate to 0', () => {
-		expect(convertBaseCodeToUnitCode("-1")).toEqual(0);
+	it('"-1" should translate to "0"', () => {
+		expect(convertBaseCodeToUnitCode("-1")).toEqual("0");
 	});
 
-	it('A null value should translate to 0', () => {
-		expect(convertBaseCodeToUnitCode(null)).toEqual(0);
+	it('A null value should translate to "0"', () => {
+		expect(convertBaseCodeToUnitCode(null)).toEqual("0");
 	});
 
-	it('An undefined value should translate to 0', () => {
-		expect(convertBaseCodeToUnitCode(undefined)).toEqual(0);
+	it('An undefined value should translate to "0"', () => {
+		expect(convertBaseCodeToUnitCode(undefined)).toEqual("0");
 	});
 });
