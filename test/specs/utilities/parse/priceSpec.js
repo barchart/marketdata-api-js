8b02b07908fa1b71ca0f9699b4be53d93e6a93a8
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-04-24 17:11:51 -0500

    Oops, uncomment tests, check code, fix code

--- test/specs/utilities/parse/priceSpec.js
+++ test/specs/utilities/parse/priceSpec.js
@@ -169,7 +169,6 @@ describe('when valid prices are parsed', () => {
 				expect(parsePrice('377.750', '2', '.', false, ',')).toEqual(377.75);
 			});
 
-			/*
 			it('parses "3,770.750" as ', () => {
 				expect(parsePrice('3,770.750', '2', '.', false, ',')).toEqual(3770.75);
 			});
@@ -193,7 +192,6 @@ describe('when valid prices are parsed', () => {
 			it('parses "3,770,000.000" as ', () => {
 				expect(parsePrice('3,770,000.000', '2', '.', false, ',')).toEqual(3770000);
 			});
-			*/
 		});
 	});
 });
