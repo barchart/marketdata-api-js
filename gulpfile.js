970fc63ae19b5e3f7573881db4bd32d3417a897e
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-05-04 11:44:22 -0500

    Remove unused gulp scripts (use @barchart/barchart-documentation now)

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,19 @@ const AWS = require('aws-sdk'),
 	browserify = require('browserify'),
 	buffer = require('vinyl-buffer'),
 	bump = require('gulp-bump'),
-	compareVersions = require('compare-versions'),
-	docsify = require('docsify-cli/lib/commands/init'),
-	file = require('gulp-file'),
 	fs = require('fs'),
 	git = require('gulp-git'),
 	gitStatus = require('git-get-status'),
 	glob = require('glob'),
 	gulp = require('gulp'),
 	jasmine = require('gulp-jasmine'),
-	jsdoc2md = require('jsdoc-to-markdown'),
 	jshint = require('gulp-jshint'),
 	merge = require('merge-stream'),
-	path = require('path'),
 	prompt = require('gulp-prompt'),
 	rename = require('gulp-rename'),
 	replace = require('gulp-replace'),
 	source = require('vinyl-source-stream'),
 	spawn = require('child_process').spawn;
-
-const docsifyTemplates = require('./docsifyTemplates');
 
 function getVersionFromPackage() {
 	return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
@@ -35,180 +28,6 @@ function getVersionForComponent() {
 function preparePathForDocs(path) {
 	return `/content/sdk/${path}`.toLocaleLowerCase();
 }
-
-function prepareLinkForDocs(path, tab) {
-	const name = path.replace(/-/g, '/');
-	return `${tab ? '\t' : ''}* [${name}](${preparePathForDocs(path)})\n`;
-}
-
-function generateReleaseNotes() {
-	const contentDir = `${__dirname}/docs/content`;
-	const releasesDir = `${contentDir}/releases`;
-	let releasesTemplate = docsifyTemplates.releasesTemplate;
-	let releaseNotes = [];
-
-	try	{
-		const releaseFiles = fs.readdirSync(releasesDir);
-		releaseNotes = releaseFiles
-			.filter((file) => file !== '_sidebar.md')
-			.map((file) => file.replace('.md', ''))
-			.sort(compareVersions)
-			.reverse();
-	} catch (e) {
-		console.log(`Unable to scan directory [ ${releasesDir} ]: ${e}`);
-	}
-
-	releaseNotes.forEach((file) => {
-		const release = fs.readFileSync(`${releasesDir}/${file}.md`).toString();
-		if (release){
-			releasesTemplate += `\n## ${file}\n${release}\n`;
-		}
-	});
-
-	releasesTemplate += '\n<!-- releases_close -->';
-
-	gulp.src([`${contentDir}/release_notes.md`])
-		.pipe(replace(/(<!-- releases_open -->(\s|.)*<!-- releases_close -->)/gm, releasesTemplate))
-		.pipe(gulp.dest(contentDir));
-}
-
-function generateDocs(inputFiles = 'lib/**/*.js') {
-	global.packageName = '@barchart/marketdata-api-js';
-
-	const docDir = `${__dirname}/docs`;
-	const contentDir = `${docDir}/content`;
-	const conceptsDir = `${contentDir}/concepts`;
-	const releasesDir = `${contentDir}/releases`;
-	const sdkDir = `${contentDir}/sdk`;
-	const template = `{{>main}}`;
-
-	let sdkReference = docsifyTemplates.sdkReference;
-	let sdkSidebar = docsifyTemplates.sdkSidebar;
-
-	return jsdoc2md.clear().then(() => {
-		return jsdoc2md.getTemplateData({
-			files: inputFiles
-		}).then((templateData) => {
-			return templateData.reduce((templateGroups, identifier) => {
-				if (!identifier.meta) {
-					return templateGroups;
-				}
-
-				const path = identifier.meta.path;
-				const arrayFilePath = path.split('lib/');
-				const filePath = 'lib-' + arrayFilePath[1].replace(/\//g, '-');
-
-				if (!templateGroups.dataByPath[filePath]) {
-					templateGroups.dataByPath[filePath] = [];
-				}
-
-				templateGroups.dataByPath[filePath].push(identifier);
-				if (!identifier.ignore) {
-					templateGroups.pathById[identifier.id] = preparePathForDocs(filePath).toLocaleLowerCase();
-				}
-
-				return templateGroups;
-			}, {dataByPath: {}, pathById: {}});
-		}).then((templateGroups) => {
-			global.pathById = templateGroups.pathById;
-
-			const keys = Object.keys(templateGroups.dataByPath).sort();
-			keys.forEach((filePath) => {
-				const data = templateGroups.dataByPath[filePath];
-
-				const output = jsdoc2md.renderSync({
-					data: data,
-					template,
-					separators: true,
-					plugin: '@barchart/dmd-plugin',
-					"global-index-format": 'md'
-				});
-
-				if (output) {
-					sdkReference += prepareLinkForDocs(filePath);
-					sdkSidebar += prepareLinkForDocs(filePath, true);
-					fs.writeFileSync(path.resolve(sdkDir, `${filePath.toLowerCase()}.md`), output);
-				}
-			});
-
-			sdkSidebar += '<!-- sdk_close -->';
-
-			fs.writeFileSync(path.resolve(contentDir, `sdk_reference.md`), sdkReference);
-
-			gulp.src([`${docDir}/_sidebar.md`], {allowEmpty: true})
-				.pipe(replace(/(<!-- sdk_open -->(\s|.)*<!-- sdk_close -->)/gm, sdkSidebar))
-				.pipe(gulp.dest(sdkDir));
-
-			gulp.src([`${docDir}/_sidebar.md`])
-				.pipe(gulp.dest(contentDir))
-				.pipe(gulp.dest(conceptsDir))
-				.pipe(gulp.dest(releasesDir));
-
-			generateReleaseNotes();
-		});
-	});
-}
-
-gulp.task('docsify', () => {
-	const isInited = fs.existsSync("./docs/index.html");
-
-	const generateStructure = new Promise((resolve, reject) => {
-		if (!isInited) {
-			const sidebar = docsifyTemplates.sidebar;
-			const docsifyConfig = docsifyTemplates.docsifyConfig;
-			const styles = docsifyTemplates.styles;
-			const indexHTMLHead = docsifyTemplates.indexHTMLHead;
-			const quickStart = docsifyTemplates.quickStart;
-			const productOverview = docsifyTemplates.productOverview;
-			const releaseNotes = docsifyTemplates.releaseNotes;
-			const coverPageTemplate = docsifyTemplates.coverPage(getVersionFromPackage());
-
-			fs.mkdirSync(`${__dirname}/docs/content/sdk`, {recursive: true});
-			fs.mkdirSync(`${__dirname}/docs/content/releases`, {recursive: true});
-
-			docsify("./docs", "", "vue");
-
-			merge(
-				gulp.src(['./docs/index.html'])
-					.pipe(replace(/(window.\$docsify.*)/g, docsifyConfig))
-					.pipe(replace(/(<\/head>)/g, indexHTMLHead))
-					.pipe(gulp.dest('./docs/')),
-
-				gulp.src(['./docs/styles/override.css'], {allowEmpty: true})
-					.pipe(file('override.css', styles))
-					.pipe(gulp.dest('./docs/styles')),
-
-				gulp.src(['docs/content/quick_start.md'], {allowEmpty: true})
-					.pipe(file('quick_start.md', quickStart))
-					.pipe(gulp.dest('./docs/content')),
-
-				gulp.src(['docs/content/product_overview.md'], {allowEmpty: true})
-					.pipe(file('product_overview.md', productOverview))
-					.pipe(gulp.dest('./docs/content')),
-
-				gulp.src(['docs/content/release_notes.md'], {allowEmpty: true})
-					.pipe(file('release_notes.md', releaseNotes))
-					.pipe(gulp.dest('./docs/content')),
-
-				gulp.src(['docs/_sidebar.md'], {allowEmpty: true})
-					.pipe(file('_sidebar.md', sidebar))
-					.pipe(gulp.dest('./docs/')),
-
-				gulp.src(['docs/_coverpage.md'], {allowEmpty: true})
-					.pipe(file('_coverpage.md', coverPageTemplate))
-					.pipe(gulp.dest('./docs/'))
-					.on("end", resolve)
-					.on("error", reject)
-			);
-		} else {
-			return resolve();
-		}
-	});
-
-	return generateStructure.then(() => {
-		return generateDocs();
-	});
-});
 
 gulp.task('ensure-clean-working-directory', (cb) => {
 	gitStatus((err, status) => {
