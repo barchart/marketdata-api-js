b2a955011d441de4b6ff39352053367a5ccfebfe
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-05-07 17:33:10 -0500

    Release. Bump version number

--- example/browser/example.js
+++ example/browser/example.js
@@ -4121,6 +4121,7 @@ module.exports = (() => {
           q.askSize = undefined;
 
           if (q.settlementPrice) {
+            q.previousSettlementPrice = q.settlementPrice;
             q.previousPrice = q.settlementPrice;
             q.settlementPrice = undefined;
           } else if (q.lastPrice) {
@@ -4278,6 +4279,11 @@ module.exports = (() => {
           q.numberOfTrades = message.numberOfTrades;
           q.previousPrice = message.previousPrice;
           q.settlementPrice = message.settlementPrice;
+
+          if (message.previousSettlementPrice) {
+            q.previousSettlementPrice = message.previousSettlementPrice;
+          }
+
           q.openPrice = message.openPrice;
           q.highPrice = message.highPrice;
           q.lowPrice = message.lowPrice;
@@ -4799,87 +4805,118 @@ module.exports = (() => {
       /**
        * @property {string} symbol - Symbol of the quoted instrument.
        * @public
+       * @readonly
        */
       this.symbol = symbol || null;
       /**
+       * @property {Profile|null} profile - Metadata regarding the quoted instrument.
+       * @public
+       * @readonly
+       */
+
+      this.profile = null;
+      /**
        * @property {string} message - Most recent DDF message to cause a this instance to mutate.
        * @public
+       * @readonly
        */
 
       this.message = null;
       /**
        * @property {string|undefined} flag - Market status, will have one of three values: "p", "s", or undefined.
        * @public
+       * @readonly
        */
 
       this.flag = null;
       /**
        * @property {string} - One of two values, "I" or "R" -- indicating delayed or realtime data, respectively.
-       * @type {null}
+       * @public
+       * @readonly
        */
 
       this.mode = null;
       /**
        * @property {string} day - One character code indicating the day of the month of the current trading session.
        * @public
+       * @readonly
        */
 
       this.day = null;
       /**
        * @property {number} dayNum - Day of the month of the current trading session.
        * @public
+       * @readonly
        */
 
       this.dayNum = 0;
       /**
        * @property {string|null} session
        * @public
+       * @readonly
        */
 
       this.session = null;
       /**
-       * @property {Date|null} lastUpdate - The most recent refresh date. Caution should be used. This date was created from hours, minutes, and seconds without regard for the current machine's timezone. As such, it is only safe to read time-related values (e.g. ```Date.getHours```, ```Date.getMinutes```, etc). Do not attempt to compare. Do not attempt to convert.
+       * @property {Date|null} lastUpdate - The most recent refresh date. Caution should be used. This date was created from hours, minutes, and seconds without regard for the client computer's timezone. As such, it is only safe to read time-related values (e.g. ```Date.getHours```, ```Date.getMinutes```, etc). Do not attempt to compare. Do not attempt to convert.
        * @public
+       * @readonly
        */
 
       this.lastUpdate = null;
       /**
-       * @property {Date|null} lastUpdate - The most recent refresh date.
+       * @property {Date|null} time - A timezone-aware version of {@link Quote#lastUpdate}. This property will only have a value when both (a) the exchange timezone is known; and (b) the client computer's timezone is known.
+       * @public
+       * @readonly
        */
 
       this.lastUpdateUtc = null;
       /**
-       * @property {number} bidPrice - top-of-book price on the buy side.
+       * @property {number} bidPrice - The top-of-book price on the buy side.
+       * @public
+       * @readonly
        */
 
       this.bidPrice = null;
       /**
-       * @property {number} bidSize - top-of-book quantity on the buy side.
+       * @property {number} bidSize - The top-of-book quantity on the buy side.
+       * @public
+       * @readonly
        */
 
       this.bidSize = null;
       /**
-       * @property {number} askPrice - top-of-book price on the sell side.
+       * @property {number} askPrice - The top-of-book price on the sell side.
+       * @public
+       * @readonly
        */
 
       this.askPrice = null;
       /**
-       * @property {number} askSize - top-of-book quantity on the sell side.
+       * @property {number} askSize - The top-of-book quantity on the sell side.
+       * @public
+       * @readonly
        */
 
       this.askSize = null;
       /**
-       * @property {number} lastPrice - most recent price (not necessarily a trade).
+       * @property {number} lastPrice - Most recent price (not necessarily a trade).
+       * @public
+       * @readonly
        */
 
       this.lastPrice = null;
       /**
-       * @property {number} tradePrice - most recent trade price.
+       * @property {number} tradePrice - Most recent trade price.
+       * @public
+       * @readonly
        */
 
       this.tradePrice = null;
       /**
-       * @property {number} tradeSize - most recent trade quantity.
+       * @property {number} tradeSize - Most recent trade quantity.
+       * @public
+       * @readonly
        */
 
       this.tradeSize = null;
@@ -4890,35 +4927,51 @@ module.exports = (() => {
 
       /**
        * @property {number} blockTrade - Most recent block trade price.
+       * @public
+       * @readonly
        */
 
       this.blockTrade = null;
       /**
-       * @property {number} settlementPrice
+       * @property {number} settlementPrice - Settlement price for current trading session.
+       * @public
+       * @readonly
        */
 
       this.settlementPrice = null;
+      /**
+       * @property {number} previousSettlementPrice - Settlement price from previous trading session.
+       * @public
+       * @readonly
+       */
+
+      this.previousSettlementPrice = null;
       this.openPrice = null;
       this.highPrice = null;
       this.lowPrice = null;
       this.volume = null;
       this.openInterest = null;
       /**
-       * @property {number} previousPrice - price from the previous session.
+       * @property {number} previousPrice - The last price from the previous trading session.
+       * @public
+       * @readonly
        */
 
       this.previousPrice = null;
       /**
-       * @property {Date|null} time - the most recent trade, quote, or refresh. this date instance stores the hours and minutes for the exchange time (without proper timezone adjustment). use caution.
+       * @property {Date|null} time - The most recent trade, quote, or refresh time. Caution should be used. This date was created from hours, minutes, and seconds without regard for the client computer's timezone. As such, it is only safe to read time-related values (e.g. ```Date.getHours```, ```Date.getMinutes```, etc). Do not attempt to compare. Do not attempt to convert.
+       * @public
+       * @readonly
        */
 
       this.time = null;
-      this.timeUtc = null;
       /**
-       * @property {Profile|null} profile - metadata regarding the quoted instrument.
+       * @property {Date|null} time - A timezone-aware version of {@link Quote#time}. This property will only have a value when both (a) the exchange timezone is known; and (b) the client computer's timezone is known.
+       * @public
+       * @readonly
        */
 
-      this.profile = null;
+      this.timeUtc = null;
       this.ticks = [];
     }
 
@@ -4942,7 +4995,7 @@ module.exports = (() => {
   'use strict';
 
   return {
-    version: '5.0.3'
+    version: '5.1.0'
   };
 })();
 
